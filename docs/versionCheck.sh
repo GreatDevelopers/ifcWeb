079c1e0cc1612c78014df38c0a49c0b355e633bf
Hardeep <hardeep.rai@gmail.com>
2025-08-06 04:48:24 +0530

    Version check

---
+++ docs/versionCheck.sh
@@ -0,0 +1,14 @@
+npm is installed.
+Version: 10.8.2
+Location: /home/nuxt/.nvm/versions/node/v20.19.4/bin/npm
+----------------------
+node is installed.
+Version: v20.19.4
+Location: /home/nuxt/.nvm/versions/node/v20.19.4/bin/node
+----------------------
+yarn is installed.
+Version: 1.22.22
+Location: /usr/local/bin/yarn
+----------------------
+Alert: pnpm is not installed.
+----------------------
