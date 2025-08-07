0973d5c0dbf3959c05ff673346db6f50d0bd5d4b
Hardeep <hardeep.rai@gmail.com>
2025-08-07 19:11:28 +0530

    Notes type

--- docs/Nuxt2.sh
+++ docs/Nuxt2.sh
@@ -1,77 +1,23 @@
-    1  sudo su -
-    2  ls
-    3  man curl
-    4  curl -v
-    5  curl --version
-    6  git --version
-    7  dpkg -s build-essential | grep Version
-    8  nvm --version
-    9  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-   10  nvm --version
-   11  export NVM_DIR="$HOME/.nvm"
-   12  source "$NVM_DIR/nvm.sh"
-   13  cat .nvm/
-   14  echo $NVM_DIR
-   15  grep -q 'nvm.sh' ~/.bashrc || {   echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc;   echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc; }
-   16  cat .bashrc 
-   17  nvm --version
-   18  nvm install 20.19.0
-   19  node -v
-   20  npm -v
-   21  npm install -g pnpn@8.15.6
-   22  pnpm -v
-   23  npm install -g pnpn@8.15.6
-   24  npm view pnpm versions
-   25  npm install -g pnpn
-   26  pnpm -v
-   27  npm install -g pnpm
-   28  pnpm -v
-   29  history > nuxt.his
-   30  joe nuxt.his 
-   31  time npm create nuxt@latest bb-ds
-   32  cd bb-ds/
-   33  ls
-   34  ls -trla
-   35  npm run dev
-   36  cd bb-ds/
-   37  npm run dev
-   38  who
-   39  logout 
-   40  adduser nuxt
-   41  ls
-   42  ls -trla
-   43  joe .profile 
-   44  joe .bashrc 
-   45  ls -trla
-   46  rm -rf .nvm .npm .nuxtrc 
-   47  ls
-   48  mkdir Nuxt2
-   49  cd Nuxt2/
-   50  mv ~/Downloads/* ,
-   51  mv ~/Downloads/* .
-   52  ls
-   53  bash versionCheck.sh 
+1. Login to a new user say nuxt. nuxt might be a normal user with no sudo power.
+2. Download following files, in a folde (say ~/hsr, in which you wish to do the installation):
+   - https://raw.githubusercontent.com/GreatDevelopers/ifcWeb/refs/heads/main/docs/versionCheck.sh
+   - https://raw.githubusercontent.com/GreatDevelopers/ifcWeb/refs/heads/main/docs/nuxt.sh
+   
+       Make sure you are in the right folder:
+       cd ~/hsr
    54  bash versionCheck.sh > Nuxt2.log
-   55  cat Nuxt2.log 
-   56  ls
    57  bash nuxt.sh >> Nuxt2.log
-   58  joe Nuxt2.log 
-   59  bash versionCheck.sh 
    60  exit
-   61  bash versionCheck.sh 
-   62  ls
-   63  cd Nuxt2/
-   64  bash versionCheck.sh 
+       Open Terminal
    65  bash versionCheck.sh >> Nuxt2.log 
-   66  history
    67  npm create nuxt@latest bb-ds
-   68  ls -trl
    69  cd bb-ds/
-   70  ls
-   71  npm run dev -- --host
-   72  ls -trl
    73  npm install @thatopen/components @thatopen/components-front @thatopen/fragments @thatopen/ui @thatopen/ui-obc
+
+   
+   71  npm run dev -- --host
+   
+
    74  ls -trl
    75  joe ~/fileChange.txt
-   76  ls -trl ~/
-   77  history > ~/his1Nuxt2.txt
+   
